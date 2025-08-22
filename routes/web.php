7e26de674649a2bedb1cb4c3e3654078c749c72b
mahmoud alwakeel <mahmoudalwakeel200@gmail.com>
2025-08-22 20:54:18 +0300

    index view and controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
 
 Route::get('/job', [JobController::class, 'index']);
