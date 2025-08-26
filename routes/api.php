5c54744c8a7fe4a94f3db3b52c13ae930092ca83
mahmoud alwakeel <mahmoudalwakeel200@gmail.com>
2025-08-26 13:52:51 +0300

    post api controller

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,9 @@
 <?php 
 
-// use Illuminate\Http\Request;
-// use App\Http\Controllers\PostController;
-// use App\Http\Controllers\CommentController;
-// use App\Http\Controllers\TagController;
-// use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\api\PostApiController;
+use Illuminate\Support\Facades\Route;
 
 
 // Restful API
-// Route::post('/blog', [PostController::class, 'create']);
-// Route::delete('/blog/{id}', [PostController::class, 'delete']);
-
-// Route::post('/comments ', [CommentController::class, 'create']);
-// Route::post('/tags', [TagController::class, 'create']);
+Route::apiResource('post', PostApiController::class);
